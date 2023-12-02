4758d9609f579ef8d25fa97692e8dc581de8ba72
ahmed-alkayyal <87127968+ahmed-alkayyal@users.noreply.github.com>
2023-12-02 19:54:01 +0200

    t

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test',function(){
+Route::get('/test01',function(){
     return "ahmed mohammed";
 });
+
